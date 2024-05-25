8ff15bbe067cbf5893bd2823389ddb86fdbe804a
Long Shihui <shihui.long@ctg.cn>
2024-05-25 18:47:07 +0800

    init

---
+++ packages/test-2/vite.config.ts
@@ -0,0 +1,16 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  mode: 'production',
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    minify: true,
+  },
+  plugins: [
+    vue(),
+})
